676fc56b7800660a95ae20a6f191dba845d2c089
Sworb <45655868+Sworb@users.noreply.github.com>
2021-12-18 11:00:45 +0100

    First upload version

---
+++ Board.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum GameState { wait, move }
+
+public class Board : MonoBehaviour
+{
+    public GameState currentState = GameState.move;
+    public int height; //8
+    public int width; //86
+    public int offset;
+    public GameObject tilePrefab;
+    public GameObject[] pieces;
+    public GameObject[,] allPieces;
+    
+    private Tile[,] allTiles;
+    
+    
+    void Start()
+    {
+        allTiles = new Tile[width, height];
+        allPieces = new GameObject[width, height];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Vector2 tempPos = new Vector2(i, j + offset);
+                GameObject tile = Instantiate(tilePrefab, tempPos, Quaternion.identity) as GameObject;
+                tile.transform.parent = this.transform;
+                tile.name = "Tile: ( " + i + ", " + j + " )";
+                
+                int pieceToPut = Random.Range(0, pieces.Length);
+                
+                int maxIterations = 0;
+                while (MatchesAt(i, j, pieces[pieceToPut]) && maxIterations < 100) {
+                    pieceToPut = Random.Range(0, pieces.Length);
+                    maxIterations++;
+                    //Debug.Log(maxIterations);
+                }
+                maxIterations = 0;
+                
+                GameObject piece = Instantiate(pieces[pieceToPut], tempPos, Quaternion.identity);
+                piece.GetComponent<Piece>().row = j;
+                piece.GetComponent<Piece>().column = i;
+                
+                piece.transform.parent = this.transform;
+                piece.name = "Piece: ( " + i + ", " + j + " )";
+                allPieces[i, j] = piece;
+            }
+        }
+    }
+
+    private bool MatchesAt(int column, int row, GameObject piece) {
+        if (column > 1 && row > 1) {
+            if (allPieces[column - 1, row].tag == piece.tag && allPieces[column - 2, row].tag == piece.tag) {
+                return true;
+            }
+            if (allPieces[column, row - 1].tag == piece.tag && allPieces[column, row - 2].tag == piece.tag) {
+                return true;
+            }
+        }
+        else if (column <= 1 || row <= 1) {
+            if (row > 1) {
+                if (allPieces[column, row - 1].tag == piece.tag && allPieces[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1) {
+                if (allPieces[column - 1, row].tag == piece.tag && allPieces[column - 2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    private void DestroyMatchAt(int column, int row) {
+        if(allPieces[column, row].GetComponent<Piece>().isMatched) {
+            Destroy(allPieces[column, row]);
+            allPieces[column, row] = null;
+        }
+    }
+
+    public void DestroyMatches() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (allPieces[i, j] != null) {
+                    DestroyMatchAt(i, j);
+                }
+            }
+        }
+        StartCoroutine(DecreaseRowCo());
+    }
+
+    private IEnumerator DecreaseRowCo() {
+        int nullCount = 0;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (allPieces[i, j] == null) {
+                    nullCount++;
+                }else if (nullCount > 0) {
+                    allPieces[i, j].GetComponent<Piece>().row -= nullCount;
+                    allPieces[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(0.4f);
+        StartCoroutine(FillBoardCo());
+    }
+
+    private void RefillBoard() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (allPieces[i, j] == null) {
+                    Vector2 tempPos = new Vector2(i, j + offset);
+                    int pieceToUse = Random.Range(0, pieces.Length);
+                    GameObject piece = Instantiate(pieces[pieceToUse], tempPos, Quaternion.identity);
+                    allPieces[i, j] = piece;
+                    piece.GetComponent<Piece>().row = j;
+                    piece.GetComponent<Piece>().column = i;
+                    piece.transform.parent = this.transform;
+                    piece.name =  "( " + i + ", " + j + " )";
+                }
+            }
+        }
+    }
+
+    private bool MachesOnBoard() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (allPieces[i, j] != null) {
+                    if (allPieces[i, j].GetComponent<Piece>().isMatched) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator FillBoardCo()
+    {
+        RefillBoard();
+        yield return new WaitForSeconds(0.5f);
+        while (MachesOnBoard()) {
+            yield return new WaitForSeconds(0.1f);
+            DestroyMatches();
+        }
+        yield return new WaitForSeconds(0.5f);
+        currentState = GameState.move;
+
+    }
+    
+}
---
+++ Piece.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    [Header("Board Variables")]
+    public int column;
+    public int row;
+    public int previousColumn;
+    public int previousRow;
+    public int targetX;
+    public int targetY;
+    public float swipeAngle = 0f;
+
+    private Board board;
+    private GameObject otherPiece;
+    private Vector2 firstTouchPos;
+    private Vector2 finTouchPos;
+    private Vector2 tempPos;
+    public bool isMatched = false;
+    public float swipeResist = 1f;
+    
+
+    private void Start() {
+        board = FindObjectOfType<Board>();
+        //targetX = (int)transform.position.x;
+        //targetY = (int)transform.position.y;
+        //row = targetY;
+        //column = targetX;
+        //previousRow = row;
+        //previousColumn = column;
+    }
+
+    private void Update() {
+        FindMatches();
+        if (isMatched) {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(1f, 1f, 1f, 0.5f);
+        }
+        targetX = column;
+        targetY = row;
+        if (Mathf.Abs(targetX - transform.position.x) > 0.1) {
+            //Move towards target
+            tempPos = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPos, 0.01f);
+            if (board.allPieces[column, row] != this.gameObject) {
+                board.allPieces[column, row] = this.gameObject;
+            }
+        }else {
+            //Set position directly
+            tempPos = new Vector2(targetX, transform.position.y);
+            transform.position = tempPos;
+        }
+        if (Mathf.Abs(targetY - transform.position.y) > 0.1) {
+            //Move towards target
+            tempPos = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPos, 0.01f);
+            if (board.allPieces[column, row] != this.gameObject) {
+                board.allPieces[column, row] = this.gameObject;
+            }
+        }else {
+            //Set position directly
+            tempPos = new Vector2(transform.position.x, targetY);
+            transform.position = tempPos;
+        }
+    }
+
+    public IEnumerator CheckMoveCo() {
+        yield return new WaitForSeconds(0.4f);
+        if (otherPiece != null) {
+            if (!isMatched && !otherPiece.GetComponent<Piece>().isMatched) {
+                otherPiece.GetComponent<Piece>().row = row;
+                otherPiece.GetComponent<Piece>().column = column;
+                row = previousRow;
+                column = previousColumn;
+                yield return new WaitForSeconds(0.3f);
+                board.currentState = GameState.move;
+            }else{
+                board.DestroyMatches();
+            }
+            otherPiece = null;
+        }
+    }
+    
+    private void OnMouseDown() {
+        if(board.currentState == GameState.move)
+            firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp() {
+        if (board.currentState == GameState.move) {
+            finTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalcAngle();
+        }
+    }
+
+    void CalcAngle() {
+        if (Mathf.Abs(finTouchPos.y - firstTouchPos.y) > swipeResist || Mathf.Abs(finTouchPos.x - firstTouchPos.x) > swipeResist) {
+            swipeAngle = Mathf.Atan2(finTouchPos.y - firstTouchPos.y, finTouchPos.x - firstTouchPos.x) * 180/Mathf.PI;
+            MovePieces();
+            board.currentState = GameState.wait;
+        }else {
+            board.currentState = GameState.move;
+        }
+    }
+
+    void MovePieces() {
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width - 1) {
+            //Right swipe
+            otherPiece = board.allPieces[column + 1, row];
+            previousRow = row;
+            previousColumn = column;
+            otherPiece.GetComponent<Piece>().column -= 1;
+            column += 1;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height - 1) {
+            //Up swipe
+            otherPiece = board.allPieces[column, row + 1];
+            previousRow = row;
+            previousColumn = column;
+            otherPiece.GetComponent<Piece>().row -= 1;
+            row += 1;
+        } else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0) {
+            //Left swipe
+            otherPiece = board.allPieces[column - 1, row];
+            previousRow = row;
+            previousColumn = column;
+            otherPiece.GetComponent<Piece>().column += 1;
+            column -= 1;
+        } else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0) {
+            //Down swipe
+            otherPiece = board.allPieces[column, row - 1];
+            previousRow = row;
+            previousColumn = column;
+            otherPiece.GetComponent<Piece>().row += 1;
+            row -= 1;
+        }
+
+        StartCoroutine(CheckMoveCo());
+    }
+
+    void FindMatches() {
+        if (column > 0 && column < board.width - 1) {
+            GameObject leftPiece1 = board.allPieces[column - 1, row];
+            GameObject rightPiece1 = board.allPieces[column + 1, row];
+            if (leftPiece1 != null && rightPiece1 != null && leftPiece1 != this.gameObject &&
+                rightPiece1 != this.gameObject) {
+                if (leftPiece1.tag == this.gameObject.tag && rightPiece1.tag == this.gameObject.tag) {
+                    leftPiece1.GetComponent<Piece>().isMatched = true;
+                    rightPiece1.GetComponent<Piece>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
+        if (row > 0 && row < board.height - 1) {
+            GameObject upPiece1 = board.allPieces[column, row + 1];
+            GameObject downPiece1 = board.allPieces[column, row - 1];
+            if (upPiece1 != null && downPiece1 != null && upPiece1 != this.gameObject &&
+                downPiece1 != this.gameObject) {
+                if (upPiece1.tag == this.gameObject.tag && downPiece1.tag == this.gameObject.tag) {
+                    upPiece1.GetComponent<Piece>().isMatched = true;
+                    downPiece1.GetComponent<Piece>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
+    }
+}
